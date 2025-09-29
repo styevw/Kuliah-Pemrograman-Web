103f96369e427694b01c6595dcd3500bc3bbb0be
Aditya Sulaiman M <adityasulaiman6@gmail.com>
2025-09-29 14:39:23 +0800

    firstproject

---
+++ Pertemuan 7 Mencari UMKM/script.js
@@ -0,0 +1,11 @@
+// Script untuk menu mobile
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenuButton = document.getElementById('mobile-menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    if (mobileMenuButton) {
+        mobileMenuButton.addEventListener('click', function() {
+            mobileMenu.classList.toggle('hidden');
+        });
+    }
+});
